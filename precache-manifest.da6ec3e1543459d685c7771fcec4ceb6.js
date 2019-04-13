85439198a9230a8b51b127da50f76428e613deb4
Daniel Don <dandons25@gmail.com>
2019-04-13 14:46:59 +0100

    Updates

---
+++ precache-manifest.da6ec3e1543459d685c7771fcec4ceb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cee53cd86c4cb49cf988",
+    "url": "/simple-react-calc/static/js/runtime~main.cee53cd8.js"
+  },
+  {
+    "revision": "1cf3dc1e43f163284b4d",
+    "url": "/simple-react-calc/static/js/main.1cf3dc1e.chunk.js"
+  },
+  {
+    "revision": "8ab5e2a4f76d9557b6cd",
+    "url": "/simple-react-calc/static/js/1.8ab5e2a4.chunk.js"
+  },
+  {
+    "revision": "1cf3dc1e43f163284b4d",
+    "url": "/simple-react-calc/static/css/main.44130fc8.chunk.css"
+  },
+  {
+    "revision": "69bd091777473509b0b246759f4ce193",
+    "url": "/simple-react-calc/index.html"
+  }
+];
